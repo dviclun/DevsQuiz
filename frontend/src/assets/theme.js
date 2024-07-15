b7325ef906cb5cda7750eab1633509aaa6836815
Daniel Vicent <daniel.vicent.luna@gmail.com>
2024-07-15 14:07:47 +0200

    LandingPage and Header components created

---
+++ frontend/src/assets/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+import Fredoka from "../assets/fonts/Fredoka.ttf";
+
+export const theme = createTheme({
+    palette: {
+        mainBlue: '#77D2F5',
+        mainBlack: '#0C0D14',
+        secondaryBlack: '#13141E',
+    }
+});
